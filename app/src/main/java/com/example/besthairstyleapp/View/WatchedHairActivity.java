4fce9e5db8f6685da7e05519ea0e04679a3839ce
tonylau1314 <tonylau131405@gmail.com>
2020-03-11 20:48:33 +0800

    add connect firebase code

--- app/src/main/java/com/example/besthairstyleapp/View/WatchedHairActivity.java
+++ app/src/main/java/com/example/besthairstyleapp/View/WatchedHairActivity.java
@@ -1,6 +1,7 @@
 package com.example.besthairstyleapp.View;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,14 +9,31 @@ import com.example.besthairstyleapp.Controller.HairServiceController;
 import com.example.besthairstyleapp.Model.HairService;
 import com.example.besthairstyleapp.R;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class WatchedHairActivity extends AppCompatActivity {
 
     HairService HairService;
     HairServiceController HairServiceController;
+    private FirebaseFirestore db=FirebaseFirestore.getInstance();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_page);
+// Write a message to the database
+        Map<String, Object> city = new HashMap<>();
+        city.put("name", "Los Angeles");
+        city.put("state", "CA");
+        city.put("country", "USA");
+
+        db.collection("cities").document("LA")
+                .set(city);
+
+
         displayCountryStyleFliter();
         displayNews();
         displayYearFliter();
